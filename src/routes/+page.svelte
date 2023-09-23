d077d6fcf5aa80090d55e9da17af2acabc33f98e
student814 <gjtjdo2000@gmail.com>
2023-09-23 15:56:41 +0900

    remix 안하고 지도 생성 및 편의성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
     import { onMount } from "svelte";
-
+    
     let container:HTMLDivElement; //지도를 담을 영역의 DOM 레퍼런스
     onMount(() => {
         const options = { //지도를 생성할 때 필요한 기본 옵션
             center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)
-            
+            level: 5 //지도의 레벨(확대, 축소 정도)  
         };
     
         const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        const mapTypeControl = new kakao.maps.MapTypeControl();
+        const zoomControl = new kakao.maps.ZoomControl();
+
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     })
 </script>
 
-<div id="map" bind:this={container} style="width1300px;height:550px;"></div>
+<div id="map" bind:this={container} style="width:100%;height:555px;"></div>
