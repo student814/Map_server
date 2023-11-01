0a07258a9922f3096cbac9ca3908cf3eb8663431
student814 <gjtjdo2000@gmail.com>
2023-11-01 21:04:33 +0900

    지도 크기2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   onMount(async() => {
         const options = { //지도를 생성할 때 필요한 기본 옵션
             center: new kakao.maps.LatLng(37.574380, 126.968850), //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)  
+            level: 10 //지도의 레벨(확대, 축소 정도)  
         };
     
         const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
